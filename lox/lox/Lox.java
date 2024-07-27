fab2f35f888263f34b201d4b8a50048cc62b208b
Ephraim Malinga <ephraim.malinga@gmail.com>
2024-07-27 20:12:26 +0300

    error function and report helper tells user some syntax error occurerd on a given line

--- lox/lox/Lox.java
+++ lox/lox/Lox.java
@@ -24,7 +24,7 @@ public class Lox {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        f(;;) {
+        for (;;) {
             System.out.print("> ");
             String line = reader.readLine();
             if (line == null) break;
@@ -35,5 +35,26 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void run(String source) {
+       Scanner scanner = new Scanner(source);
+       List<Token> tokens = scanner.scanTokens();
+
+       // for now just print the tokens
+       for (Token token : tokens) {
+        System.out.println(token);
+       }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line" + line + "] Error" + where + ": " + message
+        );
+        hadError = true;
     }
 }
