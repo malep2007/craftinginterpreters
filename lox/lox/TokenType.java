a8bdabe3c24994a4dfc0d639eb326ab90b7b3cda
Ephraim Malinga <ephraim.malinga@gmail.com>
2024-07-27 20:35:13 +0300

    created Scanner

--- lox/lox/TokenType.java
+++ lox/lox/TokenType.java
@@ -1,8 +1,7 @@
-
 /**
  * TokenType
  */
-public enum TokenType {
+enum TokenType {
 // Single-character tokens.
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
